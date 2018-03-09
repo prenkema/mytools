4b206798d431da1d77f007b6d86a2c15c36e3ab9
System Administrator <root@MBP.local>
2018-03-09 18:55:10 +0100

    #	modified:   biggest.sh

--- biggest.sh
+++ biggest.sh
@@ -1,5 +1,11 @@
 #/!bin/bash
 # command to find the biggest files (top 20)
 
-du -a / | sort -n -r | head -n 20
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+du -ah / | sort -n -r | head -n 20
 
