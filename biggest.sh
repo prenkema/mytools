ba85fc25bc9e3021ecf77b8c7523b24187fdec8c
Peter Renkema <peter.renkema@home.nl>
2018-06-12 22:36:04 +0200

    Update biggest.sh

--- biggest.sh
+++ biggest.sh
@@ -1,6 +1,6 @@
 #/!bin/bash
 # command to find the biggest files (top 20)
-
+# added a changed comment
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
