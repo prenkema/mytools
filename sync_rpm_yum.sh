dbbbdf15dce90073d396b8bca71fba6a69c2e2a2
Peter Renkema <peter.renkema@home.nl>
2018-06-28 10:18:22 +0200

    Update sync_rpm_yum.sh
    make sure script can only run as root

--- sync_rpm_yum.sh
+++ sync_rpm_yum.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+#
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
 
 yum clean all
